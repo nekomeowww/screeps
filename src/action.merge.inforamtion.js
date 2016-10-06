7f4ab0d282247dee4284529d29ea4450a54527ae
Ayaka Neko <neko@ayaka.moe>
2016-10-05 20:31:09 -0600

    Working in progress...

---
+++ src/action.merge.inforamtion.js
@@ -0,0 +1,10 @@
+//Action of Finding creep set from config file.
+var configRoom = require('config.room');
+var configSpawn = require('config.spawn');
+var functionRoomProbe = require('function.room.probe');
+
+var actionIntergrator = {
+    run: function() {
+
+    }
+}
