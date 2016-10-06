93d9389aa367d1f26c462eed1a7e8d6a775f1041
Ayaka Neko <neko@ayaka.moe>
2016-10-05 20:31:37 -0600

    new structure of the database

---
+++ src/config.room.js
@@ -0,0 +1,10 @@
+//房间配置 Room Configuration
+var configSpawn = require('config.spawn');
+var configRoom = {
+    RoomSet:[
+        {Room:"W27N26"},
+        {Room:"W28N26"}
+    ]
+}
+
+module.exports = configRoom;
